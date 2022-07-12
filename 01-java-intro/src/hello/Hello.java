0e0ad5ffa30f80859b203fe25aa8a658e6550ce6
Evgeni Genev <egenev@yahoo.com>
2022-07-12 21:29:03 +0300

    Date added

--- 01-java-intro/src/hello/Hello.java
+++ 01-java-intro/src/hello/Hello.java
@@ -1,7 +1,10 @@
 package hello;
 
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Java World !");
+        System.out.println("Today is: " + new Date());
     }
 }
